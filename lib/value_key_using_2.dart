5809148f9d455090a51b1e063a6ddf9d057b2161
Zoir I <wowdobryy@gmail.com>
2022-11-19 23:55:07 +0600

    2nd example of using value key

---
+++ lib/value_key_using_2.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class ValueKeyDemo2 extends StatefulWidget {
+  const ValueKeyDemo2({Key? key}) : super(key: key);
+
+  @override
+  State<ValueKeyDemo2> createState() => _ValueKeyDemo2State();
+}
+
+class _ValueKeyDemo2State extends State<ValueKeyDemo2> {
+  bool showEmail = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Value Key example 2'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (showEmail)
+              TextField(
+                key: ValueKey(MyObject('email')),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  icon: Icon(Icons.email),
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            TextField(
+              key: ValueKey(MyObject('email')),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                icon: Icon(Icons.people),
+                labelText: 'Username',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          shape: StadiumBorder(),
+          primary: Colors.grey,
+          textStyle: TextStyle(fontSize: 20),
+        ),
+        icon: Icon(Icons.visibility_off),
+        label: Text('remove email'),
+        onPressed: () => setState(() {
+          showEmail = false;
+        }),
+      ),
+    );
+  }
+}
+
+class MyObject {
+  final String keyString;
+
+  MyObject(this.keyString);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MyObject &&
+          runtimeType == other.runtimeType &&
+          keyString == other.keyString;
+
+  @override
+  int get hashCode => keyString.hashCode;
+}
